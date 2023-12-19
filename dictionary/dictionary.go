64d46a1cc028f5f471dcf3b1b9f4f25447d7fefc
Sala <Salambaye2001@gmail.com>
2023-12-19 15:46:31 +0100

    Ajout de dictionary

---
+++ dictionary/dictionary.go
@@ -0,0 +1,34 @@
+package dictionary
+
+import (
+	"fmt"
+	"sort"
+)
+
+type Dictionary map[string]string
+
+// Add ajoute un mot et sa définition au dictionnaire.
+func (d Dictionary) Add(word, definition string) {
+	d[word] = definition
+}
+
+// Get pour afficher la définition spécifique d'un mot
+func (d Dictionary) Get(word string) (string, bool) {
+	definition, found := d[word]
+	return definition, found
+}
+
+// Remove pour supprimer un mot du dictionnaire.
+func (d Dictionary) Remove(word string) {
+	delete(d, word)
+}
+
+// List renvoie une liste triée des mots et de leurs définitions.
+func (d Dictionary) List() []string {
+	var result []string
+	for word, definition := range d {
+		result = append(result, fmt.Sprintf("%s: %s", word, definition))
+	}
+	sort.Strings(result)
+	return result
+}
--- main.go
+++ main.go
@@ -1,5 +1,44 @@
 package main
 
+import (
+	"fmt"
+
+	"Dictionnaire/dictionary"
+)
+
 func main() {
 
+	//Création d'un dictionnaire
+	dic := make(dictionary.Dictionary)
+
+	//Ajout de quelques mots et définitions au dictionnaire
+	dic.Add("WIFI", "Le Wi-Fi est une technologie sans fil qui permet aux appareils électroniques de se connecter à Internet et de communiquer entre eux")
+	dic.Add("Valise", "Une valise est un type de bagage utilisé pour transporter des vêtements et d'autres articles personnels lors de voyages")
+	dic.Add("Informatique", "Domaine des concepts et autres techniques employées pour le traitement automatique de l’information")
+	dic.Add("Edd", "rama")
+
+	//Utilisation de Get pour afficher la définition spécifique d'un mot
+	mot_a_afficher := "WIFI"
+	definition, found := dic.Get(mot_a_afficher)
+	if found {
+		fmt.Printf("Definition de %s: %s\n", mot_a_afficher, definition)
+	} else {
+		fmt.Printf("%s ne se trouve pas dans le dictionnaire\n", mot_a_afficher)
+	}
+
+	// Utilisation de  la méthode Remove pour supprimer un mot du dictionnaire
+	motASupprimer := "Edd"
+	if _, found := dic.Get(motASupprimer); found {
+		dic.Remove(motASupprimer)
+		fmt.Printf("%s est supprimé du dictionnaire\n", motASupprimer)
+	} else {
+		fmt.Printf("%s n'est pas dans le dictionnaire, impossible de le supprimer\n", motASupprimer)
+	}
+
+	// Appel de la méthode List pour obtenir la liste triée des mots et de leurs définitions
+	wordList := dic.List()
+	fmt.Println("\nListe triée des mots et leurs définitions :")
+	for _, entry := range wordList {
+		fmt.Println(entry)
+	}
 }
